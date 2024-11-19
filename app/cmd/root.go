70bbd6e08b37d2d9c4b15105d1c8e4d35af54c0b
Mikhail Sirkin <sirkin@algont.ru>
2024-11-19 18:48:50 +0300

    hash_key и secret_key клиента вынесены из конфига во флаги

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -11,22 +11,48 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFilePath string
+	hashKey     string
+	secretKey   string
+)
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "client config file")
+	rootCmd.PersistentFlags().StringVar(&cfgFilePath, "config", "", "client config file")
+	rootCmd.PersistentFlags().StringVar(&hashKey, "hash_key", "", "key for calculate request data hash")
+	rootCmd.PersistentFlags().StringVar(&secretKey, "secret_key", "", "key to encrypt data in database")
 }
 
 func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
+	if cfgFilePath != "" {
+		viper.SetConfigFile(cfgFilePath)
 		if err := viper.ReadInConfig(); err != nil {
 			fmt.Println("Can't read config:", err)
 			os.Exit(1)
 		}
 	} else {
-		fmt.Println("You should pass the path to config file")
+		fmt.Println("You should provide the path to config file")
+		os.Exit(1)
+	}
+
+	if hashKey != "" {
+		if err := viper.BindPFlag("hash_key", rootCmd.PersistentFlags().Lookup("hash_key")); err != nil {
+			fmt.Println("Can't bind hash key flag to viper: ", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("You should provide the hash_key flag, see --help")
+		os.Exit(1)
+	}
+
+	if secretKey != "" {
+		if err := viper.BindPFlag("secret_key", rootCmd.PersistentFlags().Lookup("secret_key")); err != nil {
+			fmt.Println("Can't bind secret key flag to viper: ", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("You should provide the secret_key flag, see --help")
 		os.Exit(1)
 	}
 }
